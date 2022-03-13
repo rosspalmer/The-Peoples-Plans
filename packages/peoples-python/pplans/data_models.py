7a09989ac9e1bc355f6d64d874aa5c7e8f395eb9
Ross Palmer <c-ross.palmer@charter.com>
2022-03-12 20:56:16 -0700

    add decoding test case

--- packages/peoples-python/pplans/data_models.py
+++ packages/peoples-python/pplans/data_models.py
@@ -1,21 +1,21 @@
 
 from abc import ABC, abstractmethod
 import datetime as dt
-from json import dumps, JSONDecoder, JSONEncoder
-from typing import Any
-
+from json import JSONEncoder
+from typing import Any, Dict
 
 DATE_FMT = '%Y-%m-%dT%H:%M:%S'
 
 
 class SerializableData(ABC):
 
-    # @abstractmethod
-    # def _get_decoder(self) -> JSONDecoder:
-    #     pass
-    #
     @abstractmethod
     def _get_encoder(self) -> JSONEncoder:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def decode_function(o: Dict) -> Any:
         pass
 
     def encode(self) -> str:
@@ -29,11 +29,15 @@ class DictEncoder(JSONEncoder):
 
 class RawEvent(SerializableData):
 
-    def __init__(self, uid: str, author: str, text: str):
+    def __init__(self, uid: str, author: str, text: str, created: str = None):
         self.uid = uid
         self.author = author
         self.text = text
-        self.created = dt.datetime.now().strftime(DATE_FMT)
+        self.created = dt.datetime.now().strftime(DATE_FMT) if created is None else created
+
+    @staticmethod
+    def decode_function(o: Dict) -> Any:
+        return RawEvent(o['uid'], o['author'], o['text'], o['created'])
 
     def _get_encoder(self) -> JSONEncoder:
         return DictEncoder()
--- packages/peoples-python/pplans/tests/test_data_models.py
+++ packages/peoples-python/pplans/tests/test_data_models.py
@@ -1,10 +1,21 @@
 
 import pytest
 
+from json import loads
 from pplans.data_models import RawEvent
 
 
 class TestRawEvent:
+
+    def test_decode(self):
+
+        event_json = '{"uid":"B-5", "author":"a@b.co", "text": "hello peach", "created": "Y-M-D"}'
+        event = loads(event_json, object_hook=RawEvent.decode_function)
+
+        assert event.uid == "B-5"
+        assert event.author == "a@b.co"
+        assert event.text == "hello peach"
+        assert event.created == "Y-M-D"
 
     def test_encode(self):
 
